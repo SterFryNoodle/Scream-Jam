13b54b263fa84d117f23a845152307b46603e531
SterFryNoodle <revangcruz@yahoo.com>
2024-10-17 15:19:08 -0700

    Added enemy behavior to switch between patrolling & chasing player

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,29 +6,48 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform enemyTarget;
+    [SerializeField] Transform[] patrolPoints;
     [SerializeField] float rotationSpeed = 1f;
     [SerializeField] float chaseRange = 5f;
+    [SerializeField] float patrolSpotThreshhold = 1f;
 
     NavMeshAgent agent;
-    float distanceToTarget = Mathf.Infinity;
+    float distanceToTarget = Mathf.Infinity;    
+    bool isProvoked = false;
+    int currentPatrolIndex = 0;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        GoToNextPoint();
     }
     
     void Update()
-    {        
-        SetEnemyRange();
+    {
+        SetEnemyBehavior();
     }
 
-    void SetEnemyRange()
+    private void SetEnemyBehavior()
     {
         distanceToTarget = Vector3.Distance(enemyTarget.position, transform.position);
 
         if (distanceToTarget <= chaseRange)
         {
-            FaceTarget();
+            isProvoked = true;
             ChaseTarget();
+        }
+        else
+        {
+            isProvoked = false;
+            PatrolArea();
+        }
+    }
+
+    void ChaseTarget()
+    {
+        if (isProvoked)
+        {
+            FaceTarget();
+            agent.SetDestination(enemyTarget.position);
         }
     }
 
@@ -36,17 +55,31 @@ public class EnemyAI : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, chaseRange);
-    }
-
-    void ChaseTarget()
-    {
-        agent.SetDestination(enemyTarget.position);
-    }
+    }    
 
     void FaceTarget()
     {
         Vector3 faceDirection = (enemyTarget.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(faceDirection.x, 0, faceDirection.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+    }
+
+    void PatrolArea()
+    {        
+        if (!agent.pathPending && agent.remainingDistance <= patrolSpotThreshhold)
+        {
+            GoToNextPoint();
+        }
+    }
+
+    void GoToNextPoint()
+    {
+        if (patrolPoints.Length == 0)
+        {
+            return;
+        }
+
+        agent.SetDestination(patrolPoints[currentPatrolIndex].position);
+        currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
     }
 }
