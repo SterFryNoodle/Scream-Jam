0666a77a73d2aae10b444313b36bd86f3590339b
SterFryNoodle <revangcruz@yahoo.com>
2024-10-22 12:24:27 -0700

    updated ghost girl asset; attack scripts & attack animation added

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] int attackDamage = 25;
+
+    PlayerHealth targetStrike;
+    void Start()
+    {
+        targetStrike = FindObjectOfType<PlayerHealth>();
+    }
+
+    void AttackHitReturn()
+    {
+        if (targetStrike == null)
+        {
+            return;
+        }
+        else
+        {
+            targetStrike.PlayerTakesDamage(attackDamage);
+            //targetStrike.GetComponent<DamagedIndicator>().ShowDamageUI(); //Grabs variable as reference to the gameobject its attached to (the player),
+                                                                          // and grabs the DamagedIndicator component to access the canvas.
+        }
+    }
+}
